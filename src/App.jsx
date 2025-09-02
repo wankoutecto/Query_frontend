62a7d68c1619da1a9d3e63c5aa92a3219c96700a
justin steve <stevewankou@yahoo.fr>
2025-09-02 18:15:29 -0400

    add route /

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Homepage from "./pages/Homepage";
@@ -7,6 +7,7 @@ export default function App(){
   return (
   <div>
     <Routes>
+      <Route path="/" element={<Navigate to="/homepage" replace />} />
       <Route path="/register" element={<Register />}></Route>
       <Route path="/login" element={<Login />}></Route>
       <Route path="/homepage" element={<Homepage />}></Route>
